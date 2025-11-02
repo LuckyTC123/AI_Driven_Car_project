66f3c0c53924f07413e3d68870c55f2441af4bef
Lokesh <120460408+LuckyTC123@users.noreply.github.com>
2025-11-02 22:06:24 +0530

    Refactor Training.py for clarity and consistency

--- AI_driven_car_project/Training.py
+++ AI_driven_car_project/Training.py
@@ -1,25 +1,24 @@
-#10 steps
-#step 2 importing data information
+#importing required
 from utilis import *
 from sklearn.model_selection import train_test_split
-path = 'mydata'
+path = 'mydata' #i did generatet it using udacity simulator
 data = importDataInfo(path)
 #step 2 data visualization
 data = balaceData(data,display=False)
 
 imagespath,steering = loaddata(path,data)
 print(imagespath[0],steering[0])
-#step 4
+
 xtrain,xvalidation ,ytrain,yval =train_test_split(imagespath,steering,test_size=0.2,random_state=5)
 print(len(xtrain))
 print(len(xvalidation))
 
-#step 5
+#screating the  model and printing summary
 model =creatmodel()
 model.summary()
 
-#step 8
 history =model.fit(batchgen(xtrain,ytrain,100,1),steps_per_epoch=200,epochs=6,
                    validation_data=batchgen(xvalidation,yval,100,0),validation_steps=100)
-model.save('modelx.h5')
+model.save('model.h5')
 print('model saved')
+
